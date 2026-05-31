3cbd107de8ccaf7a81f0414aa876309d72244b67
Yichen <49024218+greyhamechuck@users.noreply.github.com>
2026-05-30 23:34:47 -0400

    Add: house-robber - submission-1

---
+++ Data Structures & Algorithms/house-robber/submission-1.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        length = len(nums)
+
+        if length ==1:
+            return nums[0]
+
+        if length ==0:
+            return 0
+        prev2 = nums[0]
+        prev1 = max(nums[0],nums[1])
+
+        for i in range(2,length):
+            current = max(prev2+nums[i],prev1)
+            prev2=prev1
+            prev1=current
+
+        return prev1
